ef9790e4cfcf0edd44f76111ac4d68d803ba1bce
DavidGrados21 <cesitar.14744@gmail.com>
2024-09-27 11:45:26 -0500

    version 1

---
+++ app/src/main/java/com/example/proyectoappv3/InicioSesionD.kt
@@ -0,0 +1,15 @@
+package com.example.proyectoappv3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InicioSesionD : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_sesion_d)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/proyectoappv3/Registro.kt
@@ -0,0 +1,14 @@
+package com.example.proyectoappv3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registro)
+    }
+}
